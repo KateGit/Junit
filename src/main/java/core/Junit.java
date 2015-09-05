0da2e654e819d80ce3a2dec761424b1df95f0e75
Kate Milrud <kmilrud@yahoo.com>
2015-09-04 19:43:30 -0700

    Version 1.2 with surefire report

--- src/main/java/core/Junit.java
+++ src/main/java/core/Junit.java
@@ -2,6 +2,12 @@ package core;
 
 public class Junit {
 
+	public static String s = "Testing";
+	public static Integer i = 777;
+	public static Boolean t = true;
+	public static Boolean f = false;
+	
+			
 	public static void main(String[] args) {
 		System.out.println("Hello Kate");
 
--- src/test/java/core/JunitTest.java
+++ src/test/java/core/JunitTest.java
@@ -12,87 +12,67 @@ import org.junit.Test;
 public class JunitTest {
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		System.out.println("BeforeClass method will be executed before first test method starts");
-	}
-
+	public static void setUpBeforeClass() throws Exception {}
+	
 	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		System.out.println("AfterClass method will be executed after last test method completed");
-	}
+	public static void tearDownAfterClass() throws Exception {}
 
 	@Before
-	public void setUp() throws Exception {
-		  System.out.println("Before method will execute before every test method");
-	}
+	public void setUp() throws Exception {}
 
 	@After
-	public void tearDown() throws Exception {
-		System.out.println("After method will execute after every test method");
-	}
+	public void tearDown() throws Exception {}
 
 	@Test
-	public void Test_01_assertEquals_Positive() {
-	       System.out.println("Test_01_assertEquals_Positive");
-	}
+	public void Test_01_assertEquals_Positive() {assertEquals("String not the same", "Testing", Junit.s);}
 	 
 
 	@Test
-	public void Test_02_assertEquals_Negative() {
+	public void Test_02_assertEquals_Negative() {assertEquals("String not the same.", "Testing 6", Junit.s);}
 
-	       System.out.println("Test_02_assertEquals_Negative");
-	       }
+	       
 	 
 
 	@Ignore
 	@Test
-	public void Test_03_assertEquals_Ignored() {
-	       System.out.println("Test_03_assertEquals_Ignored");
-	       }
+	public void Test_03_assertEquals_Ignored() {assertEquals("String not the same.", "Testing JUnit 3", Junit.s);}
+	       
 
 
 	@Test
-	public void Test_04_AssertSame_Positive() {
-	       System.out.println("Test_04_AssertSame_Positive");
-	       }
+	public void Test_04_AssertSame_Positive() {assertSame("Integer not the same.", 777, Junit.i);}
+	       
 	 
 
 	@Test
-	public void Test_05_AssertSame_Negative() {
-	       System.out.println("Test_05_AssertSame_Negative");
-	       }
+	public void Test_05_AssertSame_Negative() {assertSame("Integer not the same.", 54, Junit.i);}
+	       
 	 
 
 	@Ignore
 	@Test
-	public void Test_06_AssertSame_Ignored() {
-	       System.out.println("Test_06_AssertSame_Ignored");
-	       }
+	public void Test_06_AssertSame_Ignored() {assertSame("Integer not the same.", 56, Junit.i);}
+	       
 
 
 	@Test
-	public void Test_07_assertFalse_Positive() {
-	       System.out.println("Test_07_assertFalse_Positive");
-	       }
+	public void Test_07_assertFalse_Positive() {assertFalse("Boolean should be false", Junit.f);}
+	       
 
 
 	@Test
-	public void Test_08_assertFalse_Negative() {
-	       System.out.println("Test_08_assertFalse_Negative");
-
-	       }
+	public void Test_08_assertFalse_Negative() {assertFalse("Boolean should be false", Junit.t);}
+	       
 	 
 
 	@Test
-	public void Test_09_assertTrue_Positive() {
-	       System.out.println("Test_09_assertTrue_Positive");
-	       }
+	public void Test_09_assertTrue_Positive() {assertTrue("Boolean should be true", Junit.t);}
+	       
 	 
 
 	@Test
-	public void Test_10_assertTrue_Negative() {
-	       System.out.println("Test_10_assertTrue_Negative");
-	       }
+	public void Test_10_assertTrue_Negative() {assertTrue("Boolean should be true", Junit.f);}
+	      
 	
 
 }
